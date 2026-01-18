d3907c964a3823b27512fa612f06cfe429e0cd12
aveoxfd <krekerbruh@hotmail.com>
2026-01-18 21:30:29 +0300

    v1.2.1b

---
+++ edgefinder.py
@@ -0,0 +1,8 @@
+
+def generate_edges(g):
+    print(g)
+    return 0
+
+x = [1, 2, 3]
+
+generate_edges(x)
---
+++ header/edgefinder.h
@@ -0,0 +1,15 @@
+#ifndef EDGE_FIND
+#define EDGE_FIND
+
+#include "position.h"
+
+position* generate_edges(const position *_vertex_array, const int _vertex_count){
+    position *out = nullptr;
+    for (int i = 0; i<_vertex_count; i++){
+        for (int j =  0; j<_vertex_count; j++){
+            
+        }
+    }
+}
+
+#endif

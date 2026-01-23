711bcaa35e097005a54a0e3fc4c960237d745f28
aveoxfd <krekerbruh@hotmail.com>
2026-01-23 16:24:35 +0300

    collector update

--- compile.bat
+++ compile.bat
@@ -1,94 +1,74 @@
 @echo off
-setlocal
-set "PATH=%SystemRoot%\System32;%PATH%"
-set "TEMP=%CD%\_tmp"
-set "TMP=%SystemRoot%\Temp\vers_temp_%RANDOM%.txt"
 
-if not exist "%TEMP%" mkdir "%TEMP%"
-
-set "VERS_URL=https://raw.githubusercontent.com/aveoxfd/winwindow/main/vers"
+set "PROJECT_TEMP=%CD%\project"
+set "VERSION_URL=https://raw.githubusercontent.com/aveoxfd/winwindow/main/version.txt"
 set "LIB_URL=https://raw.githubusercontent.com/aveoxfd/winwindow/main/libwinwindow.a"
 set "LIB_TARGET=%CD%\lib\libwinwindow.a"
-set "LOCAL_VER_FILE=%CD%\local_version.txt"
+set "LOCAL_VERSION_FILE=%CD%\project\version.txt"
+set "REMOTE_VERSION_FILE=%PROJECT_TEMP%\remote_version.txt"
 
-:: read local version (if present)
-set "LOCAL_VER=0.0"
-if exist "%LOCAL_VER_FILE%" (
-  set /p LOCAL_VER=<"%LOCAL_VER_FILE%"
-  if "%LOCAL_VER%"=="" set "LOCAL_VER=0.0"
+if not exist "%PROJECT_TEMP%" (
+    mkdir "%PROJECT_TEMP%"
 )
 
-:: download remote vers to a temporary file
-curl.exe -s -L -o "%TMP%" "%VERS_URL%"
+set "LOCAL_VERSION=0"
+if exist "%LOCAL_VERSION_FILE%" (
+    for /f "tokens=2 delims==" %%A in ('findstr /i "lib_version" "%LOCAL_VERSION_FILE%"') do (
+    set "LOCAL_VERSION=%%A"
+    )
+)
+
+echo Local version = %LOCAL_VERSION%
+
+curl.exe -s -L -o "%REMOTE_VERSION_FILE%" "%VERSION_URL%"
 if errorlevel 1 (
-  echo Error: failed to download vers from %VERS_URL%
-  del "%TMP%" >nul 2>&1
-  exit /b 2
+    echo Error: failed to download remote version
+    exit /b 1
 )
-
-:: find the line containing dot_a_version
-for /f "usebackq delims=" %%L in ("%TMP%") do (
-  echo %%L | findstr /i "dot_a_version" >nul
-  if not errorlevel 1 (
-    set "line=%%L"
-    goto :gotline
-  )
+for /f "tokens=2 delims==" %%A in (
+    'findstr /i "lib_version" "%REMOTE_VERSION_FILE%"'
+) do (
+    set "REMOTE_VERSION=%%A"
 )
-echo Error: dot_a_version not found in vers file.
-del "%TMP%" >nul 2>&1
-exit /b 3
+echo Remote version = %REMOTE_VERSION%
 
-:gotline
-:: extract value after '=' and remove quotes/spaces
-for /f "tokens=2 delims==" %%A in ("%line%") do set "REMOTE_VER=%%~A"
-set "REMOTE_VER=%REMOTE_VER:"=%"
-set "REMOTE_VER=%REMOTE_VER: =%"
+::CONVERT TO NUMBERS
+call :ver2num "%LOCAL_VERSION%" LOCAL_NUM
+call :ver2num "%REMOTE_VERSION%" REMOTE_NUM
 
-:: convert versions to numbers for comparison
-call :ver2num "%LOCAL_VER%" LOCAL_NUM
-call :ver2num "%REMOTE_VER%" REMOTE_NUM
+echo Local numeric  = %LOCAL_NUM%
+echo Remote numeric = %REMOTE_NUM%
 
-echo Local numeric: %LOCAL_NUM%
-echo Remote numeric: %REMOTE_NUM%
-
-if %REMOTE_NUM% GTR %LOCAL_NUM% (
-  echo Remote is newer - downloading library...
-  if not exist "%CD%\lib" mkdir "%CD%\lib"
-  curl.exe -s -L -o "%LIB_TARGET%" "%LIB_URL%"
-  if errorlevel 1 (
-    echo Error: failed to download library.
-    del "%TMP%" >nul 2>&1
-    exit /b 5
-  )
-  echo Downloaded: %LIB_TARGET%
-  >"%LOCAL_VER_FILE%" echo %REMOTE_VER%
+::COMPARE
+if not %REMOTE_NUM% == %LOCAL_NUM% (
+    echo Updating library...
+    if exist "%LIB_TARGET%" del "%LIB_TARGET%"
+    curl.exe -s -L -o "%LIB_TARGET%" "%LIB_URL%"
+    if errorlevel 1 (
+        echo Error: failed to download library
+        exit /b 2
+    )
+    >"%LOCAL_VERSION_FILE%" echo lib_version=%REMOTE_VERSION%
 ) else (
-  echo Local is up-to-date. No download.
+    echo Library is up to date.
 )
 
-if not exist "%CD%\bin" mkdir "%CD%\bin"
-
-del "%TMP%" >nul 2>&1
-goto cmp
+g++ -O2 -DW_STATIC -static -static-libgcc -static-libstdc++ -o bin/grapp.exe eng.cpp wnd.cpp -Iheader -Llib -lwinwindow -lopengl32 -lgdi32 -luser32
+::del "%PROJECT_TEMP%\remote_version.txt"
 exit /b 0
 
-::  function: version -> number (major*1000 + minor)
+::FUNCTION
 :ver2num
-:: args: %1 = version (e.g. 1.2.3), %2 = name of output variable
+:: %1 = version, %2 = output variable
 setlocal EnableDelayedExpansion
 set "v=%~1"
-for /f "tokens=1-3 delims=." %%a in ("!v!") do (
-  set "maj=%%a"
-  set "min=%%b"
-  set "patch=%%c"
+set "maj=0"
+set "min=0"
+for /f "tokens=1-2 delims=." %%a in ("!v!") do (
+    set "maj=%%a"
+    set "min=%%b"
 )
-if "!maj!"=="" set "maj=0"
 if "!min!"=="" set "min=0"
-set /a num=(1000 * !maj!) + !min!
+set /a num=1000*!maj!+!min! 2>nul
 endlocal & set "%2=%num%"
-goto :eof
-
-
-:cmp
-g++ -O2 -DW_STATIC -static -static-libgcc -static-libstdc++ -o bin/grapp.exe eng.cpp wnd.cpp -Iheader -Llib -lwinwindow -lopengl32 -lgdi32 -luser32
 goto :eof
